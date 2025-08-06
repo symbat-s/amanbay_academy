39471f650137c36e895f111d86988654b95abae7
marksolo001 <you@example.com>
2025-08-06 14:53:48 +0500

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Banner from "./components/Banner";
 import Bannertwo from "./components/Bannertwo";
 
+
 export default function Home() {
   return (
+    <div className="mx-auto">
       <Banner />
+     <Bannertwo />
       
+    </div>
   );
 }
