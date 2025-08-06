b41a164a6c3bbe4d65f19564d79e3a9e8278e846
symbat-s <sssymbats0@gmail.com>
2025-08-06 12:02:07 +0500

    about

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Banner from "./components/Banner";
-
+import AboutBanner from "./components/AboutSection";
 export default function Home() {
   return (
+    <div className="mx-auto">
       <Banner />
+      <AboutBanner />
+    </div>
   );
 }
